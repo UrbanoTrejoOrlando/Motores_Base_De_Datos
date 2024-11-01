482f48dce27d019205ca01f3efe6f95317bc054f
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-31 21:55:49 -0600

    Tabla realizada para el gestor archive

---
+++ archive/tabla.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Cliente (
+	folio_Cliente VARCHAR(5),
+	nombre VARCHAR(50),
+	apellido VARCHAR(50),
+	email VARCHAR(30),
+	telefono VARCHAR(10)
+) ENGINE = ARCHIVE;
+
+
+INSERT INTO Cliente VALUES ('54032','Orlando','Urbano','urbanoorlando79@gmail.com','5614364142');
+INSERT INTO Cliente VALUES ('49313','Ernesto','Montiel','urbanoorlando79@gmail.com','5614364142');
